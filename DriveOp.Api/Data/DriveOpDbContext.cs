0999819bcf14bf33bb23386fabdfc14e7d9ca08c
bknthejane <159468648+bknthejane@users.noreply.github.com>
2026-09-02 20:23:18 +0200

    chore: add EF Core, DbContext and SQL Server (#2)

---
+++ DriveOp.Api/Data/DriveOpDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DriveOp.Api.Data
+{
+    public class DriveOpDbContext : DbContext
+    {
+        public DriveOpDbContext(DbContextOptions<DriveOpDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
