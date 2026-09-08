012b05265b875ddac249e5dba5abb625f062df46
Bonolo-Nth <Bonolo.Nthejane@boxfusion.io>
2026-09-08 09:52:08 +0200

    feat: add migration for JobCard municipality and composite index

--- DriveOp.Api/Migrations/20260908074854_AddMunicipalityToJobCard.Designer.cs
+++ DriveOp.Api/Migrations/20260908074854_AddMunicipalityToJobCard.Designer.cs
@@ -4,6 +4,7 @@ using DriveOp.Api.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DriveOp.Api.Migrations
 {
     [DbContext(typeof(DriveOpDbContext))]
-    partial class DriveOpDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260908074854_AddMunicipalityToJobCard")]
+    partial class AddMunicipalityToJobCard
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
